a6032526d42f9a52e4e8031880ff613dad205f33
mohaining0430 <mohaining0430@gmail.com>
2026-05-19 14:31:09 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        data.push(val);
+        int min = mins.empty() ? val : std::min(val, mins.top());
+        mins.push(min);
+    }
+    
+    void pop() {
+       if (!data.empty()) {
+        data.pop();
+        mins.pop();
+       }
+    }
+    
+    int top() {
+        return data.top();
+    }
+    
+    int getMin() {
+        return mins.top();
+    }
+
+private:
+    stack<int> data;
+    stack<int> mins;
+};
