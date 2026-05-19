cc249f8d4be6b993864aee160de78fc79f50fe15
mohaining0430 <mohaining0430@gmail.com>
2026-05-19 15:31:28 -0700

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<car> cars;
+        for (int i = 0; i < position.size(); ++i) 
+            cars.push_back(car(position[i], speed[i]));
+        std::sort(cars.begin(), cars.end(), std::greater<car>());
+
+        stack<car> res;
+        for (const auto& car: cars) {
+            if (res.empty() || 
+                (float)(target - car.position) / (float)car.speed > (float)(target - res.top().position) / (float)res.top().speed) {
+                    res.push(car);
+                }
+        }
+        return res.size();
+    }
+
+private:
+    struct car {
+        int position;
+        int speed;
+        car(int p, int s) : position(p), speed(s) {}
+        bool operator > (const car& c) const {
+            return position > c.position;
+        }
+    };
+};
