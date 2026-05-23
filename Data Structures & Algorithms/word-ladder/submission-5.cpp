a6e86789c8f958dc520157b14281846ec9898df4
mohaining0430 <mohaining0430@gmail.com>
2026-05-23 12:21:42 -0700

    Add: word-ladder - submission-5

---
+++ Data Structures & Algorithms/word-ladder/submission-5.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, vector<string>> dict;
+        for (const auto& word: wordList) {
+            for (int i = 0; i < word.size(); ++i) {
+                auto pattern = word.substr(0, i) + "*" + word.substr(i + 1);
+                dict[pattern].push_back(word);
+            }
+        }
+
+        int res = 1;
+        unordered_set<string> visited;
+        visited.insert(beginWord);
+        queue<string> q;
+        q.push(beginWord);
+
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto word = q.front();
+                if (word == endWord)
+                    return res;
+                visited.insert(word);
+                q.pop();
+                for (int j = 0; j < word.size(); ++j) {
+                    auto pattern = word.substr(0, j) + "*" + word.substr(j + 1);
+                    for (const auto neighbor : dict[pattern]) {
+                        if (!visited.contains(neighbor))
+                            q.push(neighbor);
+                    }
+                }
+            }
+            res++;
+        }
+        return 0;
+    }
+};
