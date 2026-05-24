00cf97a2eb70e5a0bb6888cb3a54d220148824a1
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 15:16:26 -0700

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    // Use bottom-up DP to avoid timeout.
+    // Can skip.
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int len = s.size();
+        vector<bool> dp(len + 1, false);
+        dp[len] = true;
+    
+        for (int i = len - 1; i >= 0; --i) {
+            for (const auto word : wordDict) {
+                if (s.substr(i, word.size()) == word) 
+                    dp[i] = dp[i + word.size()];
+                if (dp[i])
+                    break;
+            }
+        }
+        
+        return dp[0];
+    }
+};
+
+/*
+// Recursion times out for larger test cases.
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        return dfs(s, wordSet, 0);
+    }
+
+    bool dfs(string& s, unordered_set<string>& wordSet, int index) {
+        if (index == s.size()) {
+            return true;
+        }
+
+        for (int i = index; i < s.size(); ++i) {
+            auto word = s.substr(index, i - index + 1);
+            if (wordSet.contains(word)) {
+                if (dfs(s, wordSet, i + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+*/
