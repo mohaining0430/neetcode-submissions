5551b82e8d4bf1529da94128512df8926a573cbc
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 15:06:06 -0700

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(1 + amount, amount + 1);
+        dp[0] = 0;
+        for (int cur = 1; cur <= amount; ++cur) {
+            for (const auto coin : coins) {
+                if (cur >= coin) {
+                    dp[cur] = min(dp[cur], 1 + dp[cur - coin]);
+                }
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+};
