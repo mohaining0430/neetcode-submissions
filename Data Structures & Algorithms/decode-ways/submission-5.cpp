8466caae3aa6732da749e1ffdaa3e259cfe046a0
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 14:56:25 -0700

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int len = s.size();
+        vector<int> dp(len + 1, 0);         
+        dp[len] = 1;
+        dp[len - 1] = s[len - 1] == '0' ? 0 : 1;
+        for (int i = len - 2; i >= 0; --i) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+            } else if (s[i] == '1') {
+                dp[i] = dp[i + 1] + dp[i + 2];
+            } else if (s[i] == '2') {
+                if (s[i + 1] >= '0' && s[i + 1] <= '6') {
+                    dp[i] = dp[i + 1] + dp[i + 2];;
+                } else {
+                    dp[i] = dp[i + 1];
+                }
+            } else {
+                dp[i] = dp[i + 1];
+            }
+        }
+        return dp[0];
+    }
+};
