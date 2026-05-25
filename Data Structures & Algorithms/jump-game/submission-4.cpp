9c6257cc20dff1da079a84af84114b6d266a4b82
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 23:44:14 -0700

    Add: jump-game - submission-4

---
+++ Data Structures & Algorithms/jump-game/submission-4.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // Greedy
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size() - 1;
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            if (i + nums[i] >= goal)
+                goal = i;
+        }
+        return goal == 0;
+    }
+};
+
+/*
+// DFS
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        return dfs(nums, 0);
+    }
+
+    bool dfs(vector<int>& nums, int index) {
+        if (index == nums.size() - 1) // you just have to reach the last step.
+            return true;
+        for (int i = 1; i <= nums[index]; ++i) {
+            if (dfs(nums, index + i))
+                return true;
+        }
+        return false;
+    }
+};
+*/
