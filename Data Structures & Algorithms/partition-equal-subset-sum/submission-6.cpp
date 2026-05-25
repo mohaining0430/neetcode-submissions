34497e5f941f59511120ce6ea3652277fec163a0
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 21:58:37 -0700

    Add: partition-equal-subset-sum - submission-6

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (const auto num : nums)
+            sum += num;
+        if (sum % 2 != 0)
+            return false;
+        sum /= 2;
+
+        return dfs(nums, 0, 0, sum);
+    }
+
+    bool dfs(vector<int>& nums, int index, int cur_sum, int target) {
+        if (index == nums.size()) {
+            if (cur_sum == target)
+                return true;
+            return false;
+        }
+
+        if (cur_sum > target)
+            return false;
+        
+        if (dfs(nums, index + 1, cur_sum + nums[index], target) ||
+            dfs(nums, index + 1, cur_sum, target))
+            return true;
+
+        return false;
+    }
+};
