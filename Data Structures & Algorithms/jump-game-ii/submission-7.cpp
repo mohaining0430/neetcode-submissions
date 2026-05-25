6ebcf6d02004d91777ef76e78d1d88839422706d
mohaining0430 <mohaining0430@gmail.com>
2026-05-24 23:57:39 -0700

    Add: jump-game-ii - submission-7

---
+++ Data Structures & Algorithms/jump-game-ii/submission-7.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp (nums.size(), INT_MAX);
+        dp[0] = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = i + 1; j <= min(i + nums[i], (int)nums.size() - 1); ++j) {
+                dp[j] = min(dp[i] + 1, dp[j]);
+                if (dp[nums.size() - 1] != INT_MAX) {
+                    return dp[nums.size() - 1];
+                }
+            }
+        }
+        return dp[nums.size() - 1];
+    }
+};
+
+/*
+// DFS -- timed out
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int steps = 0;
+        dfs(nums, 0, steps);
+        return res == INT_MAX ? -1 : res;
+    }
+
+    void dfs(vector<int>& nums, int index, int& steps) {
+        if (index >= nums.size() - 1) {
+            res = min(res, steps);
+            return;
+        }
+
+        for (int i = 1; i <= nums[index]; ++i) {
+            steps++;
+            dfs(nums, index + i, steps);
+            steps--;
+        }
+    }
+
+private:
+    int res = INT_MAX;
+};
+*/
