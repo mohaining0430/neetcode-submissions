235f8338ce657bb3234627f6d93e31b34e456d17
mohaining0430 <mohaining0430@gmail.com>
2026-05-25 15:52:57 -0700

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        if (intervals.size() <= 1)
+            return true;
+        sort(intervals.begin(), intervals.end(), [](const Interval& i1, const Interval& i2) {
+            return i1.start < i2.start;
+        });
+        for (int i = 0; i < intervals.size() - 1; ++i)
+            if (intervals[i].end > intervals[i + 1].start)
+                return false;
+        return true;
+    }
+};
