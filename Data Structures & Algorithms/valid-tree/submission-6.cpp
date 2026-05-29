392cd20d458e43a359f74d17f16a92dc725a4318
mohaining0430 <mohaining0430@gmail.com>
2026-05-29 10:05:24 -0700

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        unordered_map<int, vector<int>> neighbors;
+        for (const auto edge : edges) {
+            neighbors[edge[0]].push_back(edge[1]);
+            neighbors[edge[1]].push_back(edge[0]);
+        }
+        unordered_set<int> visited;
+        queue<pair<int, int>> q;
+        q.push({0, -1});
+        while (!q.empty()) {
+            int cur = q.front().first;
+            visited.insert(cur);
+            int parent = q.front().second;
+            q.pop();
+            for (const auto neighbor : neighbors[cur]) {
+                if (neighbor == parent)
+                    continue;
+                if (visited.contains(neighbor))
+                    return false;
+                q.push({neighbor, cur});
+            }
+        }
+        return visited.size() == n;
+    }
+};
+
+/*
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+        unordered_set<int> visited;
+        unordered_map<int, vector<int>> neighbors;
+        for (const auto edge : edges) {
+            neighbors[edge[0]].push_back(edge[1]);
+            neighbors[edge[1]].push_back(edge[0]);
+        }
+
+        // You need to verify two things: 1) all nodes visited; 2) no cycle
+        queue<std::pair<int, int>> q;
+        q.push({0, -1});
+        while (!q.empty()) {
+            int cur = q.front().first;
+            visited.insert(cur);
+            int parent = q.front().second;
+            q.pop();
+            for (const auto neighbor : neighbors[cur]) {
+                if (neighbor == parent)
+                    continue;
+                if (visited.contains(neighbor))
+                    return false;
+                q.push({neighbor, cur});
+            }
+        }
+
+        return visited.size() == n;
+    }
+};
+*/
