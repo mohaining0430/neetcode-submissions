7f69f3d7a0cb0953fd1107c65b0f1bf1e0068420
mohaining0430 <mohaining0430@gmail.com>
2026-08-03 15:51:21 -0700

    Add: combination-target-sum - submission-12

---
+++ Data Structures & Algorithms/combination-target-sum/submission-12.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        vector<int> output;
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        dfs(nums, 0, 0, target, output, res);
+        return res;
+    }
+
+    void dfs(vector<int>& nums, int index, int cur_sum, int target, 
+            vector<int>& output, vector<vector<int>>& res) {
+        if (index == nums.size()) {
+            if (cur_sum == target) 
+                res.push_back(output);
+            return;
+        }
+
+        if (cur_sum > target)
+            return;
+
+        // You have two options:
+        // 1. adding nums[index], then next dfs starts with index (because nums[index] can be added multi times)
+        // 2. not adding nums[index], then next dfs starts with index + 1.
+        output.push_back(nums[index]);
+        dfs(nums, index, cur_sum + nums[index], target, output, res);
+        output.pop_back();
+        dfs(nums, index + 1, cur_sum, target, output, res);
+    }
+};
