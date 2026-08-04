1a8eef0ed2550ebf3574f090942479a15d786bf1
mohaining0430 <mohaining0430@gmail.com>
2026-08-04 16:15:14 -0700

    Add: word-ladder - submission-11

---
+++ Data Structures & Algorithms/word-ladder/submission-11.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_map<string, vector<string>> m;
+        for (auto word : wordList) {
+            for (int i = 0; i < word.size(); ++i) {
+                auto pattern = word.substr(0, i) + "*" + word.substr(i + 1);
+                m[pattern].push_back(word);
+            }
+        }
+
+        queue<string> q;
+        q.push(beginWord);
+        unordered_set<string> visited;
+        int level = 1;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; ++i) {
+                auto cur = q.front();
+                q.pop();
+                if (cur == endWord)
+                    return level;
+                visited.insert(cur);
+                for (int j = 0; j < cur.size(); ++j) {
+                    auto pattern = cur.substr(0, j) + "*" + cur.substr(j + 1);
+                    if (m.contains(pattern)) {
+                        for (auto neighbor : m[pattern]) {
+                            if (!visited.contains(neighbor)) {
+                                q.push(neighbor);
+                            }
+                        }
+                    }
+                }
+            }   
+            level++;  
+        }
+        return 0;
+    }
+};
