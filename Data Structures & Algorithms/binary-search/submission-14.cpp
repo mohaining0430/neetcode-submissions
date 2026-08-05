66bfe731bab241ca293049026d8dc5625e6fe64b
mohaining0430 <mohaining0430@gmail.com>
2026-08-05 13:29:30 -0700

    Add: binary-search - submission-14

---
+++ Data Structures & Algorithms/binary-search/submission-14.cpp
@@ -0,0 +1,49 @@
+// find the exact match
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return (left < nums.size() && nums[left] == target) ? left : -1;
+    }
+};
+
+/*
+// find first element that is > target
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left >= 1 && nums[left - 1] == target ? left - 1 : -1;
+    }
+};
+
+// find first element that is >= target
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else 
+                right = mid;
+        }
+        return left < nums.size() && nums[left] == target ? left : -1;
+    }
+};
+*/
