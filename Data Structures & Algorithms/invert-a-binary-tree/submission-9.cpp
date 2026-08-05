1f50a19d81f88f6483b42f7101a2d1f07dbadb71
mohaining0430 <mohaining0430@gmail.com>
2026-08-04 17:29:14 -0700

    Add: invert-a-binary-tree - submission-9

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-9.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// DFS -- iterative
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*> s;
+        if (root != nullptr)
+            s.push(root);
+        while (!s.empty()) {
+            TreeNode* cur = s.top();
+            s.pop();
+            swap(cur->left, cur->right);
+            if (cur->right != nullptr)
+                s.push(cur->right);
+            if (cur->left != nullptr)
+                s.push(cur->left);                
+        }
+        return root;
+    }
+};
+
+
+/*
+// DFS -- recursive
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        return invert(root);
+    }
+
+    TreeNode* invert(TreeNode *root) {
+        if (root == nullptr)
+            return nullptr;
+        TreeNode *left = invert(root->left);
+        TreeNode *right = invert(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
+*/
+
+
+/*
+// BFS
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        queue<TreeNode*> queue;
+        if (root != nullptr)
+            queue.push(root);
+        while (!queue.empty()) {
+            TreeNode *cur = queue.front();
+            queue.pop();
+            TreeNode *left = cur->left, *right = cur->right;
+            cur->left = right;
+            cur->right = left;
+            if (cur->left != nullptr)
+                queue.push(cur->left);
+            if (cur->right != nullptr)
+                queue.push(cur->right);            
+        }
+        return root;
+    }
+};
+*/
