ad4c55274217a97507c491576950db987aba90a1
mohaining0430 <mohaining0430@gmail.com>
2026-09-21 17:16:31 -0700

    Add: partition-to-k-equal-sum-subsets - submission-8

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-8.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        vector<bool> visited(nums.size(), false);
+        int sum = 0;
+        for (const auto num : nums)
+            sum += num;
+        if (sum % k != 0)
+            return false;
+        sum /= k;
+        sort(nums.rbegin(), nums.rend());
+        return dfs(nums, visited, k, 0, 0, sum);
+    }
+
+    bool dfs(vector<int>& nums, vector<bool>& visited, int k, int index, int cur_sum, int target) {
+        if (k == 0)
+            return true;
+        if (cur_sum == target) 
+            return dfs(nums, visited, k - 1, 0, 0, target);
+        if (index == nums.size())
+            return false;
+        if (cur_sum > target)
+            return false;
+
+        // We cannot take nums[index].
+        if (visited[index])
+            return dfs(nums, visited, k, index + 1, cur_sum, target);
+
+        // We can take nums[index]. We have two options: take it or not take it.
+        visited[index] = true;
+        if (dfs(nums, visited, k, index + 1, cur_sum + nums[index], target))
+            return true;
+        visited[index] = false;
+        if (dfs(nums, visited, k, index + 1, cur_sum, target))
+            return true;
+        return false;
+    }
+};
